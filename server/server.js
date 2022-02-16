ab88774aa8ff559aee6323c751f7f5b50b26a9cc
Elsa Lossius <elsalos@icloud.com>
2022-02-16 17:39:26 +0100

    Minimal react app and server connected

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
@@ -6,6 +7,16 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`started on http://localhost:${server.address().port}`);
 });
 
-app.get("/quiz", (req, res) => {
+app.get("/quiz/randomQuestion", (req, res) => {
   res.json({ username: "Gila og Elsa" });
+});
+
+app.use(express.static("../client/dist"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
