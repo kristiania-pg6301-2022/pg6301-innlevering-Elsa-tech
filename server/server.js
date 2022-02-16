246e9b8a499b3e243dfe4c4dc7937e99ce571a29
Elsa Lossius <elsalos@icloud.com>
2022-02-16 16:38:53 +0100

    Working server.js

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`started on http://localhost:${server.address().port}`)
+})
+
+app.get("/quiz", (req, res ) => {
+    res.json({username: "Gila og Elsa"});
+})
