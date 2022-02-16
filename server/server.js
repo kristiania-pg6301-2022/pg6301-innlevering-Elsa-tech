a43a9835a2e3f7459764fd1480cb5325c73d0a64
Gilajoanna <gilajoannajohansson@gmail.com>
2022-02-16 16:57:01 +0100

    Add prettier and husky

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`started on http://localhost:${server.address().port}`)
-})
+  console.log(`started on http://localhost:${server.address().port}`);
+});
 
-app.get("/quiz", (req, res ) => {
-    res.json({username: "Gila og Elsa"});
-})
+app.get("/quiz", (req, res) => {
+  res.json({ username: "Gila og Elsa" });
+});
